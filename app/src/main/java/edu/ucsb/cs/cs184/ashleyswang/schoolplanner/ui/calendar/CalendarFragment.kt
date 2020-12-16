218fa8b5f5df2420e9db895566b91970e91882fd
Max LR <31642273+Prothean051@users.noreply.github.com>
2020-12-15 18:54:03 -0800

    MLR - Working on RecyclerView, builds but not populating

--- app/src/main/java/edu/ucsb/cs/cs184/ashleyswang/schoolplanner/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/ashleyswang/schoolplanner/ui/calendar/CalendarFragment.kt
@@ -1,6 +1,5 @@
 package edu.ucsb.cs.cs184.ashleyswang.schoolplanner.ui.calendar
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,35 +10,90 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import edu.ucsb.cs.cs184.ashleyswang.schoolplanner.MainActivity
 import edu.ucsb.cs.cs184.ashleyswang.schoolplanner.R
-import edu.ucsb.cs.cs184.ashleyswang.schoolplanner.core.Course
+import edu.ucsb.cs.cs184.ashleyswang.schoolplanner.core.Controller
 import edu.ucsb.cs.cs184.ashleyswang.schoolplanner.core.Event
 import edu.ucsb.cs.cs184.ashleyswang.schoolplanner.core.Scope
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import java.text.SimpleDateFormat
 import java.time.DayOfWeek
 import java.time.format.TextStyle
 import java.util.*
 
 class CalendarFragment : Fragment() {
 
-    private lateinit var homeViewModel: CalendarViewModel
+    private val TAG: String = "CalendarFragment"
 
+    private lateinit var model: CalendarViewModel
+    private lateinit var controller: Controller
+    private lateinit var eventList: ArrayList<Event>
+    private lateinit var adapter: CalEventAdapter
+    private lateinit var selectedDate: String
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
+        model =
             ViewModelProviders.of(this).get(CalendarViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_calendar, container, false)
+        controller = (activity as MainActivity).controller
+        eventList = arrayListOf<Event>()
+        makeEventList()
+        var calRecyclerView = root.findViewById<RecyclerView>(R.id.calRecyclerView)
+        adapter = CalEventAdapter(eventList)
+        calRecyclerView.adapter = adapter
+        calRecyclerView.layoutManager = LinearLayoutManager(requireContext().applicationContext)
+        val dividerItemDecoration = DividerItemDecoration(
+            calRecyclerView.context,
+            (calRecyclerView.layoutManager as LinearLayoutManager).orientation
+        )
+        calRecyclerView.addItemDecoration(dividerItemDecoration)
         var calendarView = root.findViewById<CalendarView>(R.id.calendarView)
+        val sdf = SimpleDateFormat("MM/dd/yyyy")
+        selectedDate = sdf.format(Date(calendarView.date))
         calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            Toast.makeText(requireContext().applicationContext, "$month/$dayOfMonth/$year", Toast.LENGTH_SHORT).show()
+            selectedDate = "$month/$dayOfMonth/$year"
+            makeEventList()
+            Toast.makeText(
+                requireContext().applicationContext,
+                "Showing events for $selectedDate",
+                Toast.LENGTH_SHORT
+            ).show()
         }
         return root
     }
 
-    inner class CalEventAdapter (private val events: List<Event>)
+    //init {
+        //makeEventList()
+        //makeCalEventAdapter()
+    //}
+
+    private fun makeEventList() {
+        if(eventList.size > 0) eventList.clear()
+        for(term in controller.terms){
+            eventList.addAll(term.value.events.values)
+        }
+        eventList.sortBy { it.start }
+        //adapter.notifyDataSetChanged()
+    }
+
+    private fun makeCalEventAdapter() {
+        adapter = CalEventAdapter(eventList)
+        calRecyclerView.adapter = adapter
+        calRecyclerView.layoutManager = LinearLayoutManager(requireContext().applicationContext)
+        val dividerItemDecoration = DividerItemDecoration(
+            calRecyclerView.context,
+            (calRecyclerView.layoutManager as LinearLayoutManager).orientation
+        )
+        calRecyclerView.addItemDecoration(dividerItemDecoration)
+    }
+
+    inner class CalEventAdapter(private val events: List<Event>)
         : RecyclerView.Adapter<CalEventAdapter.ViewHolder>()
     {
         inner class ViewHolder(itemView: View) :
@@ -82,14 +136,17 @@ class CalendarFragment : Fragment() {
                 val month =
                     if (event.start.monthValue < 10) "0${event.start.monthValue}"
                     else event.start.monthValue.toString()
-                val dateString = "$dayOfWeek $month/$dayOfMonth"
-                val hour = event.start.hour
-                val minute = event.start.minute
-                val timeString = "$hour:$minute"
-                viewHolder.eventScope.text = dateString
-                viewHolder.eventStartTime.text = timeString
+                val year = event.start.year.toString()
+                if(selectedDate == "$dayOfMonth/$month/$year") { //Check if event on selected day
+                    val dateString = "$dayOfWeek $month/$dayOfMonth"
+                    val hour = event.start.hour
+                    val minute = event.start.minute
+                    val timeString = "$hour:$minute"
+                    viewHolder.eventScope.text = dateString
+                    viewHolder.eventStartTime.text = timeString
+                }
             } else
-                viewHolder.eventName.text = "No upcoming events or deadlines"
+                viewHolder.eventName.text = "No upcoming events"
         }
 
         // Returns the total count of items in the list
--- app/src/main/java/edu/ucsb/cs/cs184/ashleyswang/schoolplanner/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/ashleyswang/schoolplanner/ui/calendar/CalendarViewModel.kt
@@ -3,9 +3,11 @@ package edu.ucsb.cs.cs184.ashleyswang.schoolplanner.ui.calendar
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import edu.ucsb.cs.cs184.ashleyswang.schoolplanner.core.Event
+import edu.ucsb.cs.cs184.ashleyswang.schoolplanner.core.Term
 
 class CalendarViewModel : ViewModel() {
-
+    var eventsList: ArrayList<Event> = arrayListOf<Event>()
 //    private val _text = MutableLiveData<String>().apply {
 //        value = "This is calendar Fragment"
 //    }
